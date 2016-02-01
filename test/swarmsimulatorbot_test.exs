7b9bd391b90d77ea6ec5cba44005d689e4a4cd9e
Radek Molenda <rmolenda@leadformance.com>
2016-02-01 21:36:12 +0100

    There are no drones at the beginning!

--- test/swarmsimulatorbot_test.exs
+++ test/swarmsimulatorbot_test.exs
@@ -1,10 +1,21 @@
 defmodule SwarmsimulatorbotTest do
   use ExUnit.Case
+  use Hound.Helpers
   doctest Swarmsimulatorbot
 
-  test "Initial number of units" do
+  test "Initial number of units should be three" do
     Swarmsimulatorbot.start
     assert length(Swarmsimulatorbot.units) == 3
+    Swarmsimulatorbot.stop
+  end
+
+  test "having 0 drones at the beginning" do
+    Swarmsimulatorbot.start
+    text = Swarmsimulatorbot.units
+    |> Enum.at(2)
+    |> inner_text
+
+    assert text =~ ~r/Drone.*0/
     Swarmsimulatorbot.stop
   end
 end
