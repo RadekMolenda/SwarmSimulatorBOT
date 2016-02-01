fcfa98df1c4914466141cf740e01e22b99f03e2d
Radek Molenda <rmolenda@leadformance.com>
2016-02-01 20:11:28 +0100

    Add #units spec

--- test/swarmsimulatorbot_test.exs
+++ test/swarmsimulatorbot_test.exs
@@ -1,8 +1,16 @@
 defmodule SwarmsimulatorbotTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
+  use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
   doctest Swarmsimulatorbot
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup_all do
+    HTTPoison.start
+  end
+
+
+  test "Initial number of units" do
+    use_cassette "start" do
+      assert length(Swarmsimulatorbot.units) == 3
+    end
   end
 end
