9019af11e797e97e87521d93634947ff858bdf39
Radek Molenda <rmolenda@leadformance.com>
2016-02-02 11:11:50 +0100

    Introduce Cli

--- lib/swarmsimulatorbot/cli.ex
+++ lib/swarmsimulatorbot/cli.ex
@@ -2,8 +2,14 @@ defmodule Swarmsimulatorbot.Cli do
   @tick 1_000
   @screenshot_tick 60_000
 
+  def start do
+    spawn __MODULE__, :main, []
+  end
+
   def main do
     bot_pid = spawn(Swarmsimulatorbot, :start, [])
+    :timer.send_interval(@tick, bot_pid, {:grow})
+    :timer.send_interval(@screenshot_tick, bot_pid, {:screenshot, "growing.png"})
   end
 
 end
