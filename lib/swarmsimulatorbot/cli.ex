fa9ffa91a5db0eb08e8d744ba9f111f3284080aa
Radek Molenda <rmolenda@leadformance.com>
2016-02-16 22:45:07 +0100

    Grow with OTP

--- lib/swarmsimulatorbot/cli.ex
+++ lib/swarmsimulatorbot/cli.ex
@@ -2,4 +2,20 @@ defmodule Swarmsimulatorbot.Cli do
   @tick 1000
   @screenshot_tick 10000
 
+  use GenServer
+
+  def start do
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  def init(_) do
+    Process.send_after(self(), :grow, 1)
+    {:ok, nil}
+  end
+
+  def handle_info(:grow, state) do
+    Swarmsimulatorbot.dummy_grow
+    Process.send_after(self(), :grow, @tick)
+    {:noreply, state}
+  end
 end
