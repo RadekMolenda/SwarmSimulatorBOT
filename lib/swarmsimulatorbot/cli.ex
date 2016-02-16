d4e6ca6ac860c60ef772a185c51a25f64283ea9c
Radek Molenda <rmolenda@leadformance.com>
2016-02-16 22:38:17 +0100

    Remove old functions

--- lib/swarmsimulatorbot/cli.ex
+++ lib/swarmsimulatorbot/cli.ex
@@ -2,15 +2,4 @@ defmodule Swarmsimulatorbot.Cli do
   @tick 1000
   @screenshot_tick 10000
 
-  def start do
-    spawn_link __MODULE__, :main, []
-  end
-
-  def main do
-    bot_pid = Swarmsimulatorbot.start
-    :timer.send_interval(@tick, bot_pid, {:grow})
-    :timer.send_interval(@screenshot_tick, bot_pid, {:screenshot, "growing.png"})
-    bot_pid
-  end
-
 end
